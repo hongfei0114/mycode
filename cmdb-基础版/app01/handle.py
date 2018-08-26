8526ea80b4313f22d26da29f7daa2875e7265588
wangpenghong <wangpenghong@gmei.com>
2018-08-26 13:11:07 +0800

    delete cmdb-基础版

--- cmdb-基础版/app01/handle.py
+++
@@ -1,8 +0,0 @@
-
-import hashlib
-
-def md5_encrypt(username, passwd):
-    md5_obj = hashlib.md5(username.encode('utf-8'))
-    md5_obj.update(passwd.encode('utf-8'))
-    md5_passwd = md5_obj.hexdigest()
-    return md5_passwd 
